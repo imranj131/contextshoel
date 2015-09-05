1a973226a22164681b597eaa3c9b2fe664ab8941
Your Name <you@example.com>
2015-09-05 11:06:54 -0400

    storing posted steps in mongo

--- Server.js
+++ Server.js
@@ -3,6 +3,11 @@ var app     = express();
 var bodyParser = require("body-parser");
 var path = require("path");
 
+
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/pennapps';
+
 // configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -28,9 +33,61 @@ app.use('/steps/', getStepsRouter);
 
 // Handle http posts
 app.post('/post_step', function(req, res) {
-    console.log("POST:");
-    console.log(req);
     res.json("Just posted ");
+
+    mongoClient.connect(mongoUrl, function (mongoError, db) {
+
+        var date = new Date();
+
+        if (!mongoError) {
+            db.collection("steps").insertOne({
+                "date" : getCurrentDateTime(),
+                "steps" : 3
+            });
+
+            console.log("Inseted steps into db");
+        }
+
+        else {
+            db.close();
+        }
+    });
 });
+
+function getCurrentDateTime() {
+    var date = new Date();
+
+    var year = date.getFullYear();
+
+    var monthNumber = date.getMonth();
+    monthNumber++;
+    if(monthNumber < 10) {
+        monthNumber = "0" + monthNumber;
+    }
+
+    var dayNumber = date.getDate();
+    if(dayNumber < 10) {
+        dayNumber = "0" + dayNumber;
+    }
+
+    var hourNumber = date.getHours();
+    if(hourNumber < 10) {
+        hourNumber = "0" + hourNumber;
+    }
+
+    var minuteNumber = date.getMinutes();
+    if(minuteNumber < 10) {
+        minuteNumber = "0" + minuteNumber;
+    }
+
+    var secondNumber = date.getSeconds();
+    if(secondNumber < 10) {
+        secondNumber = "0" + secondNumber;
+    }
+
+    return year + "-" + monthNumber + 
+        "-" + dayNumber + " " + hourNumber + ":" + 
+        minuteNumber + ":" + secondNumber;
+}
 
 app.listen(5000);
