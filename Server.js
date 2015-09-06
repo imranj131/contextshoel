27366fdf155a4383ae9bb6a43f7b52f3a5a1ae91
Antoine Dahan <Antoine-D@users.noreply.github.com>
2015-09-06 01:02:48 -0400

    steps endpoints done

--- Server.js
+++ Server.js
@@ -3,6 +3,8 @@ var app     = express();
 var bodyParser = require("body-parser");
 var path = require("path");
 
+var http = require('http');
+var url  = require('url');
 
 var mongodb = require('mongodb');
 var mongoClient = mongodb.MongoClient;
@@ -16,7 +18,11 @@ app.get('/',function(req,res) {
 });
 
 app.get('/app',function(req,res) {
-    res.sendFile(path.join(__dirname+"/dashboard/index.html"));
+    res.sendFile(path.join(__dirname+"/dashboard.html"));
+});
+
+app.get('/old',function(req,res) {
+    res.sendFile(path.join(__dirname+"/app.html"));
 });
 
 /* Api Router */
@@ -27,18 +33,124 @@ getStepsRouter.use(function (req, res, next) {
     return;
 });
 
+
+
 // static files
 //app.use("/css", express.static(__dirname + '/css'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/assets", express.static(__dirname + '/assets'));
 
+/* expecting date variable as string with format: 2015-09-06 */
+app.use("/get_steps", function(req, res, next) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    date = "";
+    if(query.hasOwnProperty("date")) {
+        console.log(query.date);
+        date = query.date;
+    }
+
+    hour = "";
+    if(query.hasOwnProperty("hour") && !isNaN(query.hour)) {
+        console.log(query.hour);
+        hour = parseInt(query.hour);
+    }
+
+    if(date.length > 0 || hour.length > 0) {
+        
+        if(date.length > 0 && hour.length > 0) {
+            mongoClient.connect(mongoUrl, function (mongoError, db) {
+
+                if (!mongoError) {
+                    var dbRows = db.collection("steps").find({
+                        "date" : date,
+                        "hour" : hour
+                    }).toArray(function(err, items) {
+                        if(err == null) {
+                            res.json(items);
+                        }
+                        db.close();
+                    });
+                    
+                }
+                else {
+                    db.close();
+                    res.end();
+                }
+            });
+        }
+
+        else if(date.length > 0) {
+            mongoClient.connect(mongoUrl, function (mongoError, db) {
+
+                if (!mongoError) {
+                    var dbRows = db.collection("steps").find({
+                        "date" : date
+                    }).toArray(function(err, items) {
+                        if(err == null) {
+                            res.json(items);
+                        }
+                        db.close();
+                    });
+                }
+                else {
+                    db.close();
+                    res.end();
+                }
+            });
+        }
+        else {
+            res.end();
+        }
+    }
+    else {
+        res.end();
+    }
+
+    //res.end();
+});
+
+app.use("/send_steps", function(req, res, next) {
+
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+
+    if(query.hasOwnProperty("steps") && !isNaN(query.steps)){
+        console.log(query.steps);
+        console.log("sent some steps");
+
+        mongoClient.connect(mongoUrl, function (mongoError, db) {
+            if (!mongoError) {
+                db.collection("steps").insertOne({
+                    "date" : getCurrentDate(),
+                    "hour" : getCurrentHour(),
+                    "steps" : query.steps
+                });
+
+                console.log("Inseted steps into db");
+            }
+
+            else {
+                db.close();
+            }
+        });
+
+    }
+    else {
+        console.log("need steps variable");
+    }
+
+    res.end();
+    
+});
 
 // Registering the routers
 app.use('/steps/', getStepsRouter);
 
 // Handle http posts
 app.post('/post_step', function(req, res) {
-    res.json("Just posted ");
+    res.json("Just posted steps");
 
     mongoClient.connect(mongoUrl, function (mongoError, db) {
 
@@ -58,6 +170,33 @@ app.post('/post_step', function(req, res) {
         }
     });
 });
+
+function getCurrentDate() {
+    var date = new Date();
+
+    var year = date.getFullYear();
+
+    var monthNumber = date.getMonth();
+    monthNumber++;
+    if(monthNumber < 10) {
+        monthNumber = "0" + monthNumber;
+    }
+
+    var dayNumber = date.getDate();
+    if(dayNumber < 10) {
+        dayNumber = "0" + dayNumber;
+    }
+
+    return year + "-" + monthNumber + 
+        "-" + dayNumber;
+}
+
+function getCurrentHour() {
+    var date = new Date();
+    var hourNumber = date.getHours();
+    return hourNumber;
+
+}
 
 function getCurrentDateTime() {
     var date = new Date();
